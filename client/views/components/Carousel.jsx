b29c0f16551fac3aa0a0c328e7673a985c9b96ea
Brett Cunningham <bwcunninghamii@gmail.com>
2019-10-03 08:29:36 -0600

    implemented image carousel

--- client/views/components/Carousel.jsx
+++ client/views/components/Carousel.jsx
@@ -10,7 +10,7 @@ const Carousel = (props) => {
       overflow: 'hidden',
       whiteSpace: 'nowrap',
       textAlign: 'center',
-      backgroundColor: 'red',
+      backgroundColor: '#fefefe',
       width: '686px'
     },
     reviewWrapper: {
@@ -19,30 +19,49 @@ const Carousel = (props) => {
       width: '100%',
       height: '100%',
       textAlign: 'center',
-      backgroundColor: 'green',
+      backgroundColor: '#fefefe',
       margin: '0 auto'
     },
     review: {
       width: '686px',
       display: 'inline-block'
+    },
+    avatarSlider: {
+      width: '100px',
+      margin: '0 auto',
+      backgroundColor: '#fefefe',
+      overflow: 'hidden',
+    },
+    avatarWrapper: {
+      width: '100 %',
+      height: '100 %',
+      display: 'inline-block'
+    },
+    avatar: {
+      borderRadius: '50%'
     }
   }));
   const classes = useStyles();
 
   const [reviews, setReviews] = useState();
+  const [avatar, setAvatar] = useState();
 
   useEffect(() => {
     if (1) {
-      setReviews(7);
+      setReviews(8);
     }
   });
 
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const [translateValue, setTranslateValue] = useState(0);
+  const [translateAvatarValue, setTranslateAvatarValue] = useState(0);
 
   const [slideWidth, setSlideWidth] = useState(686);
   const [sliderWidth, setSliderWidth] = useState({ width: 686 });
+
+  const [avatarWidth, setAvatarWidth] = useState(100);
+  const [avatarSliderWidth, setAvatarSliderWidth] = useState(100);
 
   const getLastIndex = () => {
     if (!reviews) {
@@ -59,6 +78,7 @@ const Carousel = (props) => {
     setCurrentIndex(index);
     if (index !== 0) {
       setTranslateValue(translateValue + slideWidth);
+      setTranslateAvatarValue(translateAvatarValue + avatarWidth)
     }
   };
 
@@ -68,11 +88,12 @@ const Carousel = (props) => {
     const index = shouldResetIndex ? 0 : currentIndex + 1;
 
     if (index === 0) {
-      return setCurrentIndex(0), setTranslateValue(0);
+      return setCurrentIndex(0), setTranslateValue(0), setTranslateAvatarValue(0);
     }
 
     setCurrentIndex(currentIndex + 1);
     setTranslateValue(translateValue + -slideWidth);
+    setTranslateAvatarValue(translateAvatarValue + -avatarWidth);
   };
 
 
@@ -108,6 +129,22 @@ const Carousel = (props) => {
           <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed <br></br> do eiusmod tempor incididunt ut labore et dolre magna <br></br> aliqua."</p>
         </div>
       </div>
+      <div className={classes.avatarSlider}>
+
+        <div className={classes.avatarWrapper} style={{
+          transform: `translateX(${translateAvatarValue}px)`,
+          transition: 'transform ease-out 0.2s',
+        }}>
+          <img className={classes.avatar} src='./img/sourceImages/avatarPhoto.jpg' height="100px" width="100px" />
+          <img className={classes.avatar} src='./img/sourceImages/avatarPhoto.jpg' height="100px" width="100px" />
+          <img className={classes.avatar} src='./img/sourceImages/avatarPhoto.jpg' height="100px" width="100px" />
+          <img className={classes.avatar} src='./img/sourceImages/avatarPhoto.jpg' height="100px" width="100px" />
+          <img className={classes.avatar} src='./img/sourceImages/avatarPhoto.jpg' height="100px" width="100px" />
+          <img className={classes.avatar} src='./img/sourceImages/avatarPhoto.jpg' height="100px" width="100px" />
+
+        </div>
+      </div>
+
       <div>
         <button onClick={previousSlide}>Prev review</button>
         <button onClick={nextSlide}>Next review</button>
